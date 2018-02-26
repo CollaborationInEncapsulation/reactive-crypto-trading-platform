8ff8a576e1bfb808abfbd14fa7c2666f5bb3b4a3
Oleh Dokuka <shadowgun@i.ua>
2018-02-26 17:11:35 +0200

    initial state

---
+++ src/main/java/com/example/demo/CryptoTradingApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingApplication.class, args);
+	}
+}
